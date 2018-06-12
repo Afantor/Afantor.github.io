1843e94847a801a1b1218cdeeef8c690760a0739
Afantor <38486704+Afantor@users.noreply.github.com>
2018-06-12 12:20:39 +0800

    add new.

--- js/main.js
+++ js/main.js
@@ -1,111 +1,57 @@
-jQuery(function($) {
+$(document).ready(function() {
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
 
-    var updateArrows = function(){
-        $('.carouselGallery-right').removeClass('disabled');
-        $('.carouselGallery-left').removeClass('disabled');
-        var curIndex = $('.carouselGallery-carousel.active').data('index');
-        updateArrows.nbrOfItems = updateArrows.nbrOfItems || $('.carouselGallery-carousel').length -1;
+		if (scroll >= 50) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
 
-        curIndex === updateArrows.nbrOfItems && $('.carouselGallery-right').addClass('disabled');
-        curIndex === 0 && $('.carouselGallery-left').addClass('disabled');
-    }
-    $('.carouselGallery-carousel').on('click', function(e){
-        scrollTo = $('body').scrollTop();
-       $('body').addClass('noscroll');
-       $('body').css('position', 'fixed');
-        $('.carouselGallery-col-1, .carouselGallery-col-2').removeClass('active');
-        $(this).addClass('active');
-        showModal($(this));
-        updateArrows();
-    });
+	// Fancybox
+	$('.work-box').fancybox();
 
-    $('body').on('click', '.carouselGallery-right, .carouselGallery-left', function(e){
-        if($(this).hasClass('disabled')) return;
-        var curIndex = $('.carouselGallery-carousel.active').data('index');
-        var nextItemIndex = parseInt(curIndex+1);
-        if($(this).hasClass('carouselGallery-left')){
-            nextItemIndex-=2;
-        }
-        var nextItem = $('.carouselGallery-carousel[data-index='+nextItemIndex+']');
-       // console.log(nextItemIndex);
-        if(nextItem.length > 0){
-            $('.carouselGallery-col-1, .carouselGallery-col-2').removeClass('active');
-            $('body').find('.carouselGallery-wrapper').remove();
-            showModal($(nextItem.get(0)));
-            nextItem.first().addClass('active');
-        }
-        updateArrows();
-    });
+	// Flexslider
+	$('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+	});
 
-    var modalHtml = '';
-    showModal = function(that){
-     //   console.log(that);
-        var username = that.data('username'),
-        location = that.data('location'),
-        imagetext = that.data('imagetext'),
-        likes =  that.data('likes'),
-        imagepath = that.data('imagepath'),
-        carouselGalleryUrl = that.data('url');
-        postURL =  that.data('posturl');
+	// Page Scroll
+	var sections = $('section')
+		nav = $('nav[role="navigation"]');
 
-        maxHeight = $(window).height()-100;
+	$(window).on('scroll', function () {
+	  	var cur_pos = $(this).scrollTop();
+	  	sections.each(function() {
+	    	var top = $(this).offset().top - 76
+	        	bottom = top + $(this).outerHeight();
+	    	if (cur_pos >= top && cur_pos <= bottom) {
+	      		nav.find('a').removeClass('active');
+	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    	}
+	  	});
+	});
+	nav.find('a').on('click', function () {
+	  	var $el = $(this)
+	    	id = $el.attr('href');
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - 75
+		}, 500);
+	  return false;
+	});
 
-        if ($('.carouselGallery-wrapper').length === 0) {
-            if(typeof imagepath !== 'undefined') {
-                modalHtml = "<div class='carouselGallery-wrapper'>";
-                modalHtml += "<div class='carouselGallery-modal'><span class='carouselGallery-left'><span class='glyphicon glyphicon-menu-left'></span></span><span class='carouselGallery-right'><span class='glyphicon glyphicon-menu-right'></span></span>";
-                modalHtml += "<div class='container'>";
-                modalHtml += "<span class='iconscircle-cross glyphicon glyphicon-remove-sign'></span>";
-                modalHtml += "<div class='carouselGallery-scrollbox' style='max-height:"+maxHeight+"px'><div class='carouselGallery-modal-image'>";
-                modalHtml += "<img src='"+imagepath+"' alt='carouselGallery image'>";
-                modalHtml += "</div>";
-                modalHtml += "<div class='carouselGallery-modal-text'>";
-                modalHtml += "<span class='carouselGallery-modal-username'><a href='"+postURL+"'>"+username+"</a> </span>"
-                modalHtml += "<span class='carouselGallery-modal-location'>"+location+"</span>";
-                modalHtml += "<span class='carouselGallery-item-modal-likes'>";
-                modalHtml += "<span class='icons icon-heart'></span>";
-                modalHtml += "<a href='"+postURL+"'>"+likes+"</a>";
-                modalHtml += "</span>";
-                modalHtml += "<span class='carouselGallery-modal-imagetext'>";
-                modalHtml += "<p>"+imagetext+"</p>";
-                modalHtml += "</span></div></div></div></div></div>";
-                $('body').append(modalHtml).fadeIn(2500);
-            }
-        }
-    };
-
-    $('body').on( 'click','.carouselGallery-wrapper', function(e) {
-        if($(e.target).hasClass('.carouselGallery-wrapper')){
-            removeModal();
-        }
-    });
-    $('body').on('click', '.carouselGallery-modal .iconscircle-cross', function(e){
-        removeModal();
-    });
-
-     var removeModal = function(){
-        $('body').find('.carouselGallery-wrapper').remove();
-        $('body').removeClass('noscroll');
-        $('body').css('position', 'static');
-        $('body').animate({scrollTop: scrollTo}, 0);
-    };
-
-    // Avoid break on small devices
-    var carouselGalleryScrollMaxHeight = function() {
-        if ($('.carouselGallery-scrollbox').length) {
-            maxHeight = $(window).height()-100;
-            $('.carouselGallery-scrollbox').css('max-height',maxHeight+'px');
-        }
-    }
-    $(window).resize(function() { // set event on resize
-        clearTimeout(this.id);
-        this.id = setTimeout(carouselGalleryScrollMaxHeight, 100);
-    });
-    document.onkeydown = function(evt) {
-        evt = evt || window.event;
-        if (evt.keyCode == 27) {
-            removeModal();
-        }
-    };
-
+	// Mobile Navigation
+	$('.nav-toggle').on('click', function() {
+		$(this).toggleClass('close-nav');
+		nav.toggleClass('open');
+		return false;
+	});	
+	nav.find('a').on('click', function() {
+		$('.nav-toggle').toggleClass('close-nav');
+		nav.toggleClass('open');
+	});
+	
 });
